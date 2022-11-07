9180a0427337f08b622a67468336238936bc76ef
Mani Kandan <117351202+AbuThairDev@users.noreply.github.com>
2022-11-07 17:47:44 +0530

    Intiail

---
+++ records/js/arun.js
@@ -0,0 +1,61 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Match Records'
+    },
+    subtitle: {
+        text: 'Source: ' +
+            '<a href="" ' +
+            'target="_blank"></a>'
+    },
+    xAxis: {
+        categories: [
+            'Dabang delhi',
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        title: {
+            useHTML: true,
+            text: 'Values'
+        },
+        tickInterval: 1,
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:15px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Matches Played',
+        data: [8]
+
+    }, {
+        name: 'Wins',
+        data: [5]
+
+    }, {
+        name: 'Loss',
+        data: [2]
+
+    }, {
+        name: 'Die',
+        data: [0]
+
+    }, {
+        name: 'Points',
+        data: [27]
+
+    }]
+});
